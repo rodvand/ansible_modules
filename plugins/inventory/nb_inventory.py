1aa2df022c6b5ea1b8fb79da90b93a0799003c53
Martin Rødvand <martin@rodvand.net>
2023-04-24 20:57:09 +0200

    Formatting

--- plugins/inventory/nb_inventory.py
+++ plugins/inventory/nb_inventory.py
@@ -1496,41 +1496,41 @@ class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
             openapi = {}
 
         cached_api_version = openapi.get("info", {}).get("version")
-        
+
         if netbox_api_version != cached_api_version:
-            if '3.5' in netbox_api_version:
+            if "3.5" in netbox_api_version:
                 endpoint_url = self.api_endpoint + "/api/schema/?format=json"
             else:
                 self.api_endpoint + "/api/docs/?format=openapi"
-            openapi = self._fetch_information(
-                endpoint_url                
-            )
+            openapi = self._fetch_information(endpoint_url)
 
             with open(tmp_file, "w") as file:
                 json.dump(openapi, file)
 
         # Just for beta
-        if '3.5' in openapi["info"]["version"]:
+        if "3.5" in openapi["info"]["version"]:
             self.api_version = version.parse("3.5")
             self.allowed_device_query_parameters = [
-                p["name"] for p in openapi["paths"]["/api/dcim/devices/"]["get"]["parameters"]
+                p["name"]
+                for p in openapi["paths"]["/api/dcim/devices/"]["get"]["parameters"]
             ]
             self.allowed_vm_query_parameters = [
                 p["name"]
-                for p in openapi["paths"]["/api/virtualization/virtual-machines/"]["get"][
-                    "parameters"
-                ]
+                for p in openapi["paths"]["/api/virtualization/virtual-machines/"][
+                    "get"
+                ]["parameters"]
             ]
         else:
             self.api_version = version.parse(openapi["info"]["version"])
             self.allowed_device_query_parameters = [
-                p["name"] for p in openapi["paths"]["/api/dcim/devices/"]["get"]["parameters"]
+                p["name"]
+                for p in openapi["paths"]["/api/dcim/devices/"]["get"]["parameters"]
             ]
             self.allowed_vm_query_parameters = [
                 p["name"]
-                for p in openapi["paths"]["/api/virtualization/virtual-machines/"]["get"][
-                    "parameters"
-                ]
+                for p in openapi["paths"]["/api/virtualization/virtual-machines/"][
+                    "get"
+                ]["parameters"]
             ]
 
     def validate_query_parameter(self, parameter, allowed_query_parameters):
